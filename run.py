634e51fecd50fcb2e398cdbf9413a9c210b9f704
Sergey <katkov2021@gmail.com>
2025-05-06 01:59:08 +0300

    0.0.0.2

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+
+from aiogram import Bot, Dispatcher,F
+from aiogram.filters import CommandStart,Command
 from aiogram.types import Message
 
-# Убедитесь, что TOKEN указан в config.py
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
@@ -13,6 +13,14 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer("1")
+
+@dp.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer("hell")
+
+@dp.message(F.text == 'How are u)')
+async def how_are_you(message: Message):
+    await message.answer('ok!')
 
 async def main():
     await dp.start_polling(bot)
